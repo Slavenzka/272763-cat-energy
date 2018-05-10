c45cabba419e7c3716a28578075e13c95c18e35e
Ivan <proekt.energetika@gmail.com>
2018-05-10 10:25:55 +0300

    Создал и применил векторный спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -15,6 +17,7 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
+    .pipe(minify())
     .pipe(server.stream());
 });
 
